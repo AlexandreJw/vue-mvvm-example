bcb643a2152489e76f505bea523666e83a4736c5
tommy <tommy@find.com>
2018-10-30 01:08:48 +0800

    增加v-model的匹配，并加入到watcher中 dom update的事件队列 只更新最后一次

--- compile.js
+++ compile.js
@@ -17,10 +17,27 @@ Compile.prototype = {
   compileElement:function (child, vm) {
     var reg = /\{\{(.*)\}\}/;
     ///获取{{}}
+    //节点类型为元素
+    if (child.nodeType === 1) {
+      var attr = child.attributes;
+      // 解析属性
+      for (var i = 0; i < attr.length; i++) {
+        if (attr[i].nodeName == 'v-model') {
+          var name = attr[i].nodeValue; // 获取v-model绑定的属性名
+          child.addEventListener('input', function (e) {
+            // 给相应的data属性赋值，进而触发该属性的set方法
+            //再批处理 渲染元素
+            vm[name] = e.target.value;
+          });
+          // node.value = vm[name]; // 将data的值赋给该node
+          new Watcher(vm, child, name, 'value');
+        }
+      };
+    }
     if(child.nodeType == 3) {
       if(reg.test(child.nodeValue) || reg.test(child.innerHTML)){
         var name = RegExp.$1
-        new watcher(vm, child, name, 'nodeValue')
+        new Watcher(vm, child, name, 'nodeValue')
       }
     }
     return child
--- watcher.js
+++ watcher.js
@@ -1,14 +1,27 @@
-function watcher(vm, node, name, nodeType) {
+var uid = 0
+
+function Watcher (vm, node, name, type) {
+  //new Watcher(vm, node, 'test', 'nodeValue');
   Dep.target = this
-  this.vm = vm
-  this.node  = node
-  this.name = name
-  this.nodeType = nodeType
+  this.name = name //text
+  this.node = node //当前的节点
+  this.id = ++uid
+  this.vm = vm //vm
+  this.type = type //nodeValue
   this.update()
   Dep.target = null
 }
-watcher.prototype = {
-  update:function () {
-    this.node[this.nodeType] = this.vm[this.name]
+Watcher.prototype = {
+  update: function () {
+    this.get()
+    batcher = new Batcher()
+    batcher.push(this)
+  },
+  cb: function () {
+    this.node[this.type] = this.value // 订阅者执行相应操作
+  },
+  // 获取data的属性值
+  get: function () {
+    this.value = this.vm[this.name] //触发相应属性的get
   }
 }
